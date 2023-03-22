c31bb4f4c0b7e35a096a8c0aaf2ea37299581fe2
syboks-coder <58122033+syboks-coder@users.noreply.github.com>
2023-03-23 00:16:48 +0200

    created manufacturer and year entity and DAO classes

---
+++ src/main/java/com/syboks/vehiclesearch/dao/ManufactureYearDAO.java
@@ -0,0 +1,7 @@
+package com.syboks.vehiclesearch.dao;
+
+import com.syboks.vehiclesearch.entity.ManufactureYear;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ManufactureYearDAO extends JpaRepository<ManufactureYear, Integer> {
+}
---
+++ src/main/java/com/syboks/vehiclesearch/entity/Manufacturer.java
@@ -0,0 +1,22 @@
+package com.syboks.vehiclesearch.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="manufacturer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Manufacturer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name="manufacturer_name")
+    private String manufacturerName;
+    @Column(name="country_of_origin")
+    private String countryOfOrigin;
+
+}
